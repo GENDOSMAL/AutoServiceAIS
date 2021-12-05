08f56cb9b3073d0d9872d222ba19c8cec2446c12
Gena <malkerovg99@gmail.com>
2021-12-05 18:02:08 +0300

    init commit 2

---
+++ src/autoServiceApi/src/main/java/com/genderfrender/autoServiceApi/AutoServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.genderfrender.autoServiceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoServiceApiApplication.class, args);
+	}
+
+}
